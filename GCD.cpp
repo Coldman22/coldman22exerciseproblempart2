e4fc9b844d0aa5983c419abb794499a95748842a
Shishir Tambe <93042473+Coldman22@users.noreply.github.com>
2022-07-27 18:30:20 +0530

    Add files via upload
    In this C++ problem we are going to learn about flow control.
    This problem specifies on how to find the GCD of numbers.

---
+++ GCD.cpp
@@ -0,0 +1,24 @@
+/*GCD (Greatest Common Divisor ) also known as the Highest Common Factor for a set of two numbers is largest integer that can exactly divide both numbers(without a remainder). There are different ways to calculate GCD of two integers(for both positive integers and nagative integers) using loops and decision making statement.*/
+//gcd for both positive numbers and negative numbers.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1,n2;
+    cout<<"Enter 2 numbers: ";
+    cin>>n1>>n2;
+
+    n1= (n1>0)? n1: -n1;
+    n2= (n2>0)? n2: -n2;
+
+    while (n1 != n2)
+    {
+        if(n1 > n2)
+           n1 -= n2;
+        else
+           n2 -= n1;
+    }
+    cout<<"GCD ="<<n1;
+    return 0;
+}
